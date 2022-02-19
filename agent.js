2d7c9edf4b5e9452b7037fa4f1cb892507c01080
Artem Danilov <danilov.arrtem@gmail.com>
2022-02-19 16:22:56 +0300

    add parse falg methode

--- agent.js
+++ agent.js
@@ -2,6 +2,44 @@ const Msg = require('./msg')
 // Подключение модуля разбора сообщений от сервера
 const readline = require('readline')
 
+
+const Flags = {
+    ftl50: {x: -50, y: 39}, ftl40: {x: -40, y: 39},
+    ftl30: {x: -30, y: 39}, ftl20: {x: -20, y: 39},
+    ftl10: {x: -10, y: 39}, ft0: {x: 0, y: 39},
+    ftr10: {x: 10, y: 39}, ftr20: {x: 20, y: 39},
+    ftr30: {x: 30, y: 39}, ftr40: {x: 40, y: 39},
+    ftr50: {x: 50, y: 39}, fbl50: {x: -50, y: -39},
+    fbl40: {x: -40, y: -39}, fbl30: {x: -30, y: -39},
+    fbl20: {x: -20, y: -39}, fbl10: {x: -10, y: -39},
+    fb0: {x: 0, y: -39}, fbr10: {x: 10, y: -39},
+    fbr20: {x: 20, y: -39}, fbr30: {x: 30, y: -39},
+    fbr40: {x: 40, y: -39}, fbr50: {x: 50, y: -39},
+    flt30: {x:-57.5, y: 30}, flt20: {x:-57.5, y: 20},
+    flt10: {x:-57.5, y: 10}, fl0: {x:-57.5, y: 0},
+    flb10: {x:-57.5, y: -10}, flb20: {x:-57.5, y: -20},
+    flb30: {x:-57.5, y: -30}, frt30: {x: 57.5, y: 30},
+    frt20: {x: 57.5, y: 20}, frt10: {x: 57.5, y: 10},
+    fr0: {x: 57.5, y: 0}, frb10: {x: 57.5, y: -10},
+    frb20: {x: 57.5, y: -20}, frb30: {x: 57.5, y: -30},
+    fglt: {x:-52.5, y: 7.01}, fglb: {x:-52.5, y:-7.01},
+    gl: {x:-52.5, y: 0}, gr: {x: 52.5, y: 0}, fc: {x: 0, y: 0},
+    fplt: {x: -36, y: 20.15}, fplc: {x: -36, y: 0},
+    fplb: {x: -36, y:-20.15}, fgrt: {x: 52.5, y: 7.01},
+    fgrb: {x: 52.5, y:-7.01}, fprt: {x: 36, y: 20.15},
+    fprc: {x: 36, y: 0}, fprb: {x: 36, y:-20.15},
+    flt: {x:-52.5, y: 34}, fct: {x: 0, y: 34},
+    frt: {x: 52.5, y: 34}, flb: {x:-52.5, y: -34},
+    fcb: {x: 0, y: -34}, frb: {x: 52.5, y: -34},
+    distance(p1, p2) {
+        return Math.sqrt((p1.x-p2.x)**2+(p1.y-p2.y)**2)
+    },
+}
+
+const parseFlagFromArray = (flagArray) => {
+    return flagArray.reduce((acc, curr)=> acc + curr ,'')
+}
+
 // Подключение модуля ввода из командной строки
 class Agent {
     constructor() {
@@ -51,48 +89,18 @@ class Agent {
         if (p[1]) this.id = p[1] // id игрока
     }
 
+    // cmd === see||sense_body
     analyzeEnv(msg, cmd, p) { // Анализ сообщения
-        const Flags = {
-            ftl50: {x: -50, y: 39}, ftl40: {x: -40, y: 39},
-            ftl30: {x: -30, y: 39}, ftl20: {x: -20, y: 39},
-            ftl10: {x: -10, y: 39}, ft0: {x: 0, y: 39},
-            ftr10: {x: 10, y: 39}, ftr20: {x: 20, y: 39},
-            ftr30: {x: 30, y: 39}, ftr40: {x: 40, y: 39},
-            ftr50: {x: 50, y: 39}, fbl50: {x: -50, y: -39},
-            fbl40: {x: -40, y: -39}, fbl30: {x: -30, y: -39},
-            fbl20: {x: -20, y: -39}, fbl10: {x: -10, y: -39},
-            fb0: {x: 0, y: -39}, fbr10: {x: 10, y: -39},
-            fbr20: {x: 20, y: -39}, fbr30: {x: 30, y: -39},
-            fbr40: {x: 40, y: -39}, fbr50: {x: 50, y: -39},
-            flt30: {x:-57.5, y: 30}, flt20: {x:-57.5, y: 20},
-            flt10: {x:-57.5, y: 10}, fl0: {x:-57.5, y: 0},
-            flb10: {x:-57.5, y: -10}, flb20: {x:-57.5, y: -20},
-            flb30: {x:-57.5, y: -30}, frt30: {x: 57.5, y: 30},
-            frt20: {x: 57.5, y: 20}, frt10: {x: 57.5, y: 10},
-            fr0: {x: 57.5, y: 0}, frb10: {x: 57.5, y: -10},
-            frb20: {x: 57.5, y: -20}, frb30: {x: 57.5, y: -30},
-            fglt: {x:-52.5, y: 7.01}, fglb: {x:-52.5, y:-7.01},
-            gl: {x:-52.5, y: 0}, gr: {x: 52.5, y: 0}, fc: {x: 0, y: 0},
-            fplt: {x: -36, y: 20.15}, fplc: {x: -36, y: 0},
-            fplb: {x: -36, y:-20.15}, fgrt: {x: 52.5, y: 7.01},
-            fgrb: {x: 52.5, y:-7.01}, fprt: {x: 36, y: 20.15},
-            fprc: {x: 36, y: 0}, fprb: {x: 36, y:-20.15},
-            flt: {x:-52.5, y: 34}, fct: {x: 0, y: 34},
-            frt: {x: 52.5, y: 34}, flb: {x:-52.5, y: -34},
-            fcb: {x: 0, y: -34}, frb: {x: 52.5, y: -34},
-            distance(p1, p2) {
-                return Math.sqrt((p1.x-p2.x)**2+(p1.y-p2.y)**2)
-            },
-        }
-        if (cmd === 'see') {
-            // console.log(`msg - ${msg}\n p - ${p}\n`)
-            for(let i=0; i < p.length;i++) {
-                console.log(`${p[i]}`)
-                for(let j=0; j < p[i].length;j++) {
 
+        if (cmd === 'see') {
+            const visibleFlags = p.filter(x => x.cmd && (x.cmd.p[0] === 'f' || x.cmd.p[0] === 'g'))
+            // console.log(`msg - ${msg}\n p - ${p}\n`)
+            for(let i=0; i < visibleFlags.length;i++) {
+                const flagName =  parseFlagFromArray(visibleFlags[i].cmd.p);
+                console.log(Flags[flagName]);
+                for(let j=0; j < p[i].length;j++) {
                 }
             }
-
         }
     }
 

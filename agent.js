692c3783ba2964334787ac8758ba9962027303a3
Artem Danilov <danilov.arrtem@gmail.com>
2022-02-26 17:06:18 +0300

    перед рефактором

--- agent.js
+++ agent.js
@@ -3,6 +3,7 @@ const Msg = require('./msg')
 const readline = require('readline')
 
 const orientation = require('./orientation')
+
 // Подключение модуля ввода из командной строки
 class Agent {
     constructor(teamName) {
@@ -66,39 +67,8 @@ class Agent {
 
     // cmd === see||sense_body
     analyzeEnv(msg, cmd, p) { // Анализ сообщения
-
-        if (cmd === 'sense_body') {
-            for (const sense of p.filter(x => x.cmd)) {
-                this.sense_body[sense.cmd] = sense.p;
-            }
-        }
-        if (cmd === 'see') {
-            const visibleFlags = p.filter(x => x.cmd && (x.cmd.p[0] === 'f' || x.cmd.p[0] === 'g'))
-            const visiblePlayers = p.filter(x => x.cmd && (x.cmd.p[0] === 'p'))
-
-            let coords = undefined;
-
-            //TODO разобраться с NAN
-            let uefa= 0;
-            if (visibleFlags.length >= 3) {
-                uefa= 3;
-                coords = orientation.orientationWithThreeFlag(visibleFlags[0],visibleFlags[1],visibleFlags[2])
-            } else if (visibleFlags.length === 2) {
-                uefa= 2
-                coords = orientation.orientationWithTwoFlag(visibleFlags[0], visibleFlags[1])
-            } else if (visibleFlags.length === 1) {
-                console.log('need more flags')
-            }
-
-            this.coords = coords;
-            console.log(this.teamName, uefa, 'my coords:', coords?.x, coords?.y);
-
-            visiblePlayers.forEach((player)=>{
-                let coords = orientation.orientationWithThreeFlag(...orientation.getFlagsFromObject(player, visibleFlags.slice(1)));
-                // coords.x = coords.x
-
-                console.log('its ' + this.teamName + 'Other player coords:', coords.x, coords.y);
-            })
+        if (cmd === 'see' && this.run) {
+            this.getAction(p)
         }
     }
 
@@ -122,9 +92,6 @@ class Agent {
             }
         }, 500)
     }
-
-
-
 
 
 }

031e2aabdf5024022b336d5d9cc2d87c5ce9204c
Artem Danilov <danilov.arrtem@gmail.com>
2022-03-03 21:49:30 +0300

    Changes by anton

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
-const Agent = require('./agent'); // Импорт агента
-const VERSION = 7; // Версия сервера
+const Agent = require('./agent') // Импорт агента
+const VERSION = 7 // Версия сервера
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-getCoords();
-
-function createAgent(teamName, x, y, goalie) {
-    let agent = new Agent(goalie); // Создание экземпляра агента
-    require('./socket')(agent); //Настройка сокета
-    agent.init(teamName, VERSION);
+function createAgent(teamName, speed, coords, position, goalie) {
+    let agent = new Agent(speed, teamName, position); // Создание экземпляра агента
+    console.log('agent', agent)
+    require('./socket')(agent, teamName, VERSION, goalie) //Настройка сокета
     setTimeout(() => {
-        agent.socketSend("move", `${x} ${y}`)
-    }, 100)
+        agent.socketSend("move", `${position === 'r' ? '-' : ''}${coords}`)
+        //agent.socketSend("move", `${x} ${y}`)
+    }, 20)
 }
 
-function getCoords() {
-    rl.question('X ', (x) =>
-    {
-        rl.question('Y ', (y) =>
-        {
-            rl.question('TeamName ', (teamName) =>
-            {
-                rl.question('goalie? (y/n) ', (goalie) => {
-                    createAgent(teamName, x, y, goalie);
-                    rl.close();
-                })
-            })
-        })
-    })
-}
+createAgent("biba", 0, "-10 -5", 'l')
+createAgent("biba", 0, "-15 5", 'l')
+createAgent("boba", 0, "15 0", 'r', true)

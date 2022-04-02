c8e20d5d390005a323c7296f8ff90a9581bf0f40
Artem Danilov <danilov.arrtem@gmail.com>
2022-04-02 15:33:39 +0300

    lab6

--- app.js
+++ app.js
@@ -20,7 +20,73 @@ function createAgent(teamName, coords, position, goalie) {
     // }, 20)
 }
 
-createAgent("biba", "-12 -15", 'l', false)
-// createAgent("biba", "-25 10", 'l', false)
-// createAgent("boba", "51 -8", 'r', false)
-createAgent("boba", "52.5 0", 'r', true)
+// createAgent("biba", "-12 -15", 'l', false)
+// // createAgent("biba", "-25 10", 'l', false)
+// // createAgent("boba", "51 -8", 'r', false)
+// createAgent("boba", "52.5 0", 'r', true)
+setTimeout(()=> {
+    createAgent('Biba', '-10 0', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba', '-5 -25', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba',  '-5 25', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba', '-15 -15', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba', '-15 15', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba', '-25 -15', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba', '-25 15', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba', '-35 -25', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba', '-35 0', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba',  '-35 25', 'l', false)
+},100)
+setTimeout(()=> {
+    createAgent('Biba','-50 0','l', true)
+},100)
+setTimeout(()=> {
+    createAgent('Boba', '10 0', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba', '5 25', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba', '5 -25', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba', '15 -15', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba', '15 15', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba', '25 15', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba', '25 -15', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba', '35 -25', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba','35 0', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba','35 25', 'r', false)
+},100)
+setTimeout(()=> {
+    createAgent('Boba', '50 0', 'r', true)
+},100)
